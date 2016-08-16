e4bf6c0df974b1c087341a0e315c177d6ba93bc6
Pius Wanjiru <piusmuriithi1@gmail.com>
2016-08-16 13:10:42 +0300

    populates the 'users' table using random data form the faker package

--- app/database/seeds/DatabaseSeeder.php
+++ app/database/seeds/DatabaseSeeder.php
@@ -11,8 +11,8 @@ class DatabaseSeeder extends Seeder {
 	{
 		Eloquent::unguard();
 
-		// $this->call('UserTableSeeder');
-		$this->call('PaintingsTableSeeder');
+	    $this->call('UserTableSeeder');
+		//$this->call('PaintingsTableSeeder');
 	}
 
 }
